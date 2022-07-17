814143200583f0f9d2f41eee9b05c49a4d75ad0c
LHThaii <astolfo0812@gmail.com>
2022-07-18 02:43:57 +0700

    Update CreateTable.sql
    UpdateTable

--- CreateTable.sql
+++ CreateTable.sql
@@ -22,4 +22,33 @@ CREATE TABLE [Lecturer] (
 	GroupID VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES [Group](GroupID)
 	)
 
+	CREATE TABLE [Group_lecturer](
+	LectureID INT FOREIGN KEY REFERENCES [Lecturer](LecturerID),
+	GroupID VARCHAR(10)NOT NULL FOREIGN KEY REFERENCES[Group](GroupID)
+	)
+	CREATE TABLE [Group_course](
+	CourseID INT FOREIGN KEY REFERENCES [Course](CourseID),
+	GroupID VARCHAR(10)NOT NULL FOREIGN KEY REFERENCES[Group](GroupID)
+	)
+	CREATE TABLE [Lecturer_Course](
+	LectureID INT FOREIGN KEY REFERENCES [Lecturer](LecturerID),
+	CourseID INT FOREIGN KEY REFERENCES [Course](CourseID)
+	)
+	
+	CREATE TABLE Assessment(
+	AssessmentID INT PRIMARY KEY,
+	AssessmentName VARCHAR(150),
+	WEIGHT FLOAT,
+	CourseID INT FOREIGN KEY REFERENCES [Course](CourseID)
+	)
+
+	CREATE TABLE RESULT(
+	RESULTID INT PRIMARY KEY,
+	StudentID VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES [Student](StudentID),
+	AssessmentID INT FOREIGN KEY REFERENCES [Assessment](AssessmentID),
+	CourseID INT FOREIGN KEY REFERENCES [Course](CourseID),
+	Score FLOAT
+	)
+	
+	
 
