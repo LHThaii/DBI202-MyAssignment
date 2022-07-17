ebfc951e30eeba5146da48e08f55226ca5c85c4f
LHThaii <astolfo0812@gmail.com>
2022-07-18 02:42:41 +0700

    UpdateTable
    UpdateTable

--- CreateTable.sql
+++ CreateTable.sql
@@ -1,53 +1,25 @@
-CREATE TABLE [Students](
+CREATE TABLE [Student](
 	StudentID VARCHAR(10) PRIMARY KEY,
 	StudentName NVARCHAR(30) NOT NULL,
-	DoB date NOT NULL,
-	Address VARCHAR(30) NOT NULL,
-	PhoneNumber VARCHAR(10)
+	gender BIT,
+	DoB DATE NOT NULL,
+	Address VARCHAR(30) NOT NULL	
 )
-CREATE TABLE [Courses] (
+CREATE TABLE [Course] (
 	CourseID INT PRIMARY KEY,
-	CourseCode VARCHAR(30) NOT NULL,
-	CourseName NVARCHAR(30) NOT NULL,
-	 
+	CourseName NVARCHAR(30) NOT NULL
 )
-CREATE TABLE [Lecturers] (
-	LectureID INT PRIMARY KEY,
-	LectureName NVARCHAR (30) NOT NULL,
-	LectureEmail NVARCHAR(30) NOT NULL,
-	LecturePhone NVARCHAR(10)
+CREATE TABLE [Lecturer] (
+	LecturerID INT PRIMARY KEY,
+	LecturerName NVARCHAR (30) NOT NULL
 	)
 	CREATE TABLE [Group] (
-	StudentID VARCHAR(10)NOT NULL FOREIGN KEY REFERENCES[Students](StudentID),
 	GroupID VARCHAR(10) PRIMARY KEY,
-	GroupName NVARCHAR(30) NOT NULL,
-	CourseID INT FOREIGN KEY REFERENCES Courses(CourseID),
-	LectureID INT FOREIGN KEY REFERENCES Lecturers(LectureID),
-    StartDate date, 
-	EndDate date 
+	GroupName NVARCHAR(30) NOT NULL
 )
-CREATE TABLE [Assessment](
-	CourseID INT FOREIGN KEY REFERENCES Courses(CourseID),
-	Category VARCHAR(30) NOT NULL PRIMARY KEY,
-	[Type] NVARCHAR(30) not null CHECK ([type] in ('quiz','on-going','pratical exam','final exam')),
-	Part INT NOT NULL,
-	[Weight] FLOAT NOT NULL,
-	[Completion Criteria] INT NOT NULL,
-	Duration VARCHAR (30) NOT NULL,
-	LO VARCHAR(30),
-	[Question Type] VARCHAR(150) NOT NULL,
-	[No Question] INT,
-	[Knowledge and Skill] VARCHAR(150),
-	[Grading Guide] VARCHAR(150),
-	[Note] VARCHAR (150)
+	CREATE TABLE [Group_student](
+	StudentID VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES [Student](StudentID),
+	GroupID VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES [Group](GroupID)
 	)
 
 
-CREATE TABLE [Detailed Result](
-	StudentID VARCHAR(10) NOT NULL FOREIGN KEY REFERENCES[Students](StudentID),
-	Category VARCHAR(30) NOT NULL FOREIGN KEY REFERENCES[Assessment](Category),
-	Item NVARCHAR(30) PRIMARY KEY,
-	[Weight] INT NOT NULL,
-	Value FLOAT NOT NULL,
-	Comment NVARCHAR(30)
-)
---
+++ Insert_Value.sql
@@ -0,0 +1,44 @@
+--- STUDENT
+INSERT INTO Student (StudentID,StudentName,gender,DoB,Address)
+VALUES ('HE160','Le Hoang Thai',1,'2002-12-08','Hoa Binh');
+
+INSERT INTO Student (StudentID,StudentName,gender,DoB,Address)
+VALUES ('HE161','Nguyen Duc Huy',1,'2002-12-15','Lang Son');
+
+INSERT INTO Student (StudentID,StudentName,gender,DoB,Address)
+VALUES ('HE162','Nguyen Huy Minh',1,'2002-08-04','Ha Noi');
+
+INSERT INTO Student (StudentID,StudentName,gender,DoB,Address)
+VALUES ('HE163','Nguyen Hoang Hieu',1,'2002-06-05','Bac Giang');
+
+
+INSERT INTO Student (StudentID,StudentName,gender,DoB,Address)
+VALUES ('HE164','Chu Lan Anh',0,'2002-01-08','Cao Bang');
+
+
+
+---- LECTURER
+INSERT INTO Lecturer(LecturerID,LecturerName)
+VALUES (1,'SonNT');
+INSERT INTO Lecturer(LecturerID,LecturerName)
+VALUES (2,'HaiLT');
+INSERT INTO Lecturer(LecturerID,LecturerName)
+VALUES (3,'TuanVM');
+INSERT INTO Lecturer(LecturerID,LecturerName)
+VALUES (4,'BanTQ');
+
+
+----COURSE
+INSERT INTO Course (CourseID,CourseName)
+VALUES (1,'LAB211');
+
+INSERT INTO Course (CourseID,CourseName)
+VALUES (2,'DBI202');
+
+INSERT INTO Course (CourseID,CourseName)
+VALUES (3,'CSD201');
+
+INSERT INTO Course (CourseID,CourseName)
+VALUES (4,'JPD113');
+
+
